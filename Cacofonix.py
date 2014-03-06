4f72fc1e0d9acd0a1aca5e037eaf766ec661ddb1
Aaron Schein <scheinaj@gmail.com>
2014-03-06 16:26:39 -0500

    fixed spacing

--- Cacofonix.py
+++ Cacofonix.py
@@ -10,50 +10,50 @@ EVENTS = ["Stone Curling", "Stone Skating", "Underwater Stone Weaving", "Synchro
 UNITS = ["points", "laps", "baskets", "points"]
 
 def create_flavor_statement(team, event, lead):
-	# TODO: Make more variants
-	return "What a day in %s! Currently in the lead is %s by %d %s."%(EVENTS[event].lower(), TEAMS[team], lead, UNITS[event])
+    # TODO: Make more variants
+    return "What a day in %s! Currently in the lead is %s by %d %s."%(EVENTS[event].lower(), TEAMS[team], lead, UNITS[event])
 
 class Cacofonix:
-	def __init__(self, port, server_ip, server_port):
-		self.port = port
-		self.server = xmlrpclib.ServerProxy("%s:%d"%(server_ip, server_port))
+    def __init__(self, port, server_ip, server_port):
+        self.port = port
+        self.server = xmlrpclib.ServerProxy("%s:%d"%(server_ip, server_port))
 
-	def set_score(self, event, score):
-		# TODO get back ack/error
-		self.server.set_score(event, score)
+    def set_score(self, event, score):
+        # TODO get back ack/error
+        self.server.set_score(event, score)
 
-	def increment_medal_tally(self, team, medal):
-		# TODO get back ack/error
-		self.server.increment_medal_tally(team, medal)
+    def increment_medal_tally(self, team, medal):
+        # TODO get back ack/error
+        self.server.increment_medal_tally(team, medal)
 
 def main(port=8001, server_ip='http://localhost', server_port=8000):
-	print "main"
-	fonix = Cacofonix(port = port, server_ip = server_ip, server_port = server_port)
+    print "main"
+    fonix = Cacofonix(port = port, server_ip = server_ip, server_port = server_port)
 
-	while(True):
-		# rn.seed(10)
+    while(True):
+        # rn.seed(10)
 
-		# time.sleep(rn.randint(5))
-		event = rn.randint(len(EVENTS))
-		team = rn.randint(len(TEAMS))
-		lead = rn.randint(100)
-		score = create_flavor_statement(team, event, lead)
+        # time.sleep(rn.randint(5))
+        event = rn.randint(len(EVENTS))
+        team = rn.randint(len(TEAMS))
+        lead = rn.randint(100)
+        score = create_flavor_statement(team, event, lead)
 
-		print score
-		fonix.set_score(EVENTS[event], score)
+        print score
+        fonix.set_score(EVENTS[event], score)
 
-		if rn.random() > 0.5:
-			# choose three teams
-			medal_winners = rn.choice(len(TEAMS), 3, replace=False)
+        if rn.random() > 0.5:
+            # choose three teams
+            medal_winners = rn.choice(len(TEAMS), 3, replace=False)
 
-			team = TEAMS[rn.randint(len(TEAMS))]
-			medal = MEDALS[rn.randint(len(MEDALS))]
+            team = TEAMS[rn.randint(len(TEAMS))]
+            medal = MEDALS[rn.randint(len(MEDALS))]
 
-			[fonix.increment_medal_tally(TEAMS[team], MEDALS[medal]) for medal, team in enumerate(medal_winners)]
+            [fonix.increment_medal_tally(TEAMS[team], MEDALS[medal]) for medal, team in enumerate(medal_winners)]
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
 
 
--- StoneTablet.py
+++ StoneTablet.py
@@ -8,72 +8,72 @@ TEAMS = ["Gaul", "Rome", "Carthage", "Greece", "Persia"]
 EVENTS = ["Stone Curling", "Stone Skating", "Underwater Stone Weaving", "Synchronized Stone Swimming"]
 
 class StoneTablet:
-	def __init__(self, port, server_ip, server_port, teams=[], events=[]):
-		self.port = port
-		self.teams = teams
-		self.events = events
-		self.server = xmlrpclib.ServerProxy("%s:%d"%(server_ip, server_port))
+    def __init__(self, port, server_ip, server_port, teams=[], events=[]):
+        self.port = port
+        self.teams = teams
+        self.events = events
+        self.server = xmlrpclib.ServerProxy("%s:%d"%(server_ip, server_port))
 
-	# Methods for client-pull architecture
+    # Methods for client-pull architecture
 
-	def get_medal_tally(self, team):
-		return self.server.get_medal_tally(team)
+    def get_medal_tally(self, team):
+        return self.server.get_medal_tally(team)
 
-	def get_score(self, event):
-		return self.server.get_score(event)
+    def get_score(self, event):
+        return self.server.get_score(event)
 
-	def pull(self):
-		for team in self.teams:
-			print self.get_medal_tally(team)
-		for event in self.events:
-			print self.get_score(event)
+    def pull(self):
+        for team in self.teams:
+            print self.get_medal_tally(team)
+        for event in self.events:
+            print self.get_score(event)
 
-	# Methods for server-push architecture
+    # Methods for server-push architecture
 
-	class ListenerFunctions:
-		def print_medal_tally_for_team(self, medal_tally):
-			print medal_tally
-		def print_score_for_event(self, score):
-			print score
+    class ListenerFunctions:
+        def print_medal_tally_for_team(self, medal_tally):
+            print medal_tally
+        def print_score_for_event(self, score):
+            print score
 
-	def get_my_IP(self):
-		# TODO
-		return '127.0.0.1'
+    def get_my_IP(self):
+        # TODO
+        return '127.0.0.1'
 
-	def register_with_server(self):
-		print self.server.register_client(port = self.port, events = self.events, teams = self.teams)
+    def register_with_server(self):
+        print self.server.register_client(port = self.port, events = self.events, teams = self.teams)
 
-	def serve(self):
-		callback_server = SimpleXMLRPCServer("%s:%d"%(self.get_my_IP(), self.port))
-		callback_server.register_introspection_functions()
-		self.register_with_server()
-		callback_server.serve_forever()
+    def serve(self):
+        callback_server = SimpleXMLRPCServer("%s:%d"%(self.get_my_IP(), self.port))
+        callback_server.register_introspection_functions()
+        self.register_with_server()
+        callback_server.serve_forever()
 
 def main(port, teams = ["Gaul"], events = ["Stone Curling"], server_ip='http://localhost', server_port=8000, client_pull = True, pull_rate = 0.0001):
-	client = StoneTablet(port, server_ip, server_port, teams, events)
+    client = StoneTablet(port, server_ip, server_port, teams, events)
 
-	# Client-pull architecture
-	while(client_pull):
-		client.pull()
-		time.sleep(pull_rate)
+    # Client-pull architecture
+    while(client_pull):
+        client.pull()
+        time.sleep(pull_rate)
 
-	# Server-push architecture
-	client.serve()
+    # Server-push architecture
+    client.serve()
 
 if __name__ == "__main__":
-	port = rn.randint(8002, 9000)
-	num_teams = rn.randint(1, len(TEAMS)+1)
-	num_events = rn.randint(1, len(EVENTS)+1)
-	print rn.choice(len(TEAMS), num_teams, replace=False)
-	fav_teams = [TEAMS[t] for t in rn.choice(len(TEAMS), num_teams, replace=False)]
-	fav_events = [EVENTS[e] for e in rn.choice(len(EVENTS), num_events, replace=False)]
+    port = rn.randint(8002, 9000)
+    num_teams = rn.randint(1, len(TEAMS)+1)
+    num_events = rn.randint(1, len(EVENTS)+1)
+    print rn.choice(len(TEAMS), num_teams, replace=False)
+    fav_teams = [TEAMS[t] for t in rn.choice(len(TEAMS), num_teams, replace=False)]
+    fav_events = [EVENTS[e] for e in rn.choice(len(EVENTS), num_events, replace=False)]
 
-	main(port=port, teams=fav_teams, events=fav_events)
-
-
+    main(port=port, teams=fav_teams, events=fav_events)
 
 
-		
+
+
+        
 
 
 
