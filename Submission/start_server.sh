8819616d3488dac47d5d06079f9b0f4a02d09b14
Aaron Schein <scheinaj@gmail.com>
2014-04-04 18:39:47 -0400

    blah

--- Submission/start_server.sh
+++
@@ -1,8 +0,0 @@
-# Change to correct directory containing source files
-src_dir=.
-
-# Uncomment for testing on local
-run_locally=False  #'True' or 'False'
-server_port=8000
-
-python $src_dir/ObelixServer.py --run_locally $run_locally --serport $server_port
